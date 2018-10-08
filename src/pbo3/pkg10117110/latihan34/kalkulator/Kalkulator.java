1f873a6d42317b72b21defa62dbe016d38eeb6b3
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-10-08 23:36:38 +0700

    first commit

---
+++ src/pbo3/pkg10117110/latihan34/kalkulator/Kalkulator.java
@@ -0,0 +1,18 @@
+
+package pbo3.pkg10117110.latihan34.kalkulator;
+
+//import java.util.Scanner;
+
+
+public class Kalkulator {
+
+    public double value1,value2;
+
+    public double tambahBilangan(){ return value1+value2;}
+    public double kurangBiangan(){ return value1-value2;}
+    public double kaliBilangan(){ return value1*value2;}
+    public double bagiBilangan(){ return value1/value2;}
+    public double sisaBilangan(){ return value1%value2;}
+    
+    
+}
---
+++ src/pbo3/pkg10117110/latihan34/kalkulator/PBO310117110Latihan34Kalkulator.java
@@ -0,0 +1,46 @@
+
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Perhitungan Seperti Kalkulator Sederhana Dengan Menggunakan Konsep OOP
+ */
+package pbo3.pkg10117110.latihan34.kalkulator;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+public class PBO310117110Latihan34Kalkulator {
+
+
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        Kalkulator kal = new Kalkulator();
+
+        System.out.println("====Aplikasi Kalkulator Bilangan====");
+        System.out.print("Masukkan Angka ke-1 : ");
+        kal.value1 = scn.nextDouble();
+        System.out.print("Masukkan Angka ke-2 : ");
+        kal.value2 = scn.nextDouble();
+
+        //format dua digit di belakang koma
+        DecimalFormat df = new DecimalFormat("###.##");
+
+        System.out.println("\nHasil Penjumlahan : "+kal.tambahBilangan());
+        System.out.println("Hasil Pengurangan : "+kal.kurangBiangan());
+        System.out.println("Hasil Perkalian : "+kal.kaliBilangan());
+        System.out.println("Hasil Pembagian : "+df.format(kal.bagiBilangan()));
+        System.out.println("Hasil Sisa : "+kal.sisaBilangan());
+        
+
+        
+        
+    }
+    
+}
